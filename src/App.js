cd5c68930e8ecb94b802d3f341820cdb07c76a2b
Nith21 <vknitheesh01@gmail.com>
2023-05-12 01:21:21 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import "./App.css";
 
 function App() {
   const date = new Date();
-  // const days = [
-  //   "SUNDAY",
-  //   "MONDAY",
-  //   "TUESDAY",
-  //   "WEDNESDAY",
-  //   "THURSDAY",
-  //   "FRIDAY",
-  //   "SATURDAY",
-  // ];
+  const days = [
+    "SUNDAY",
+    "MONDAY",
+    "TUESDAY",
+    "WEDNESDAY",
+    "THURSDAY",
+    "FRIDAY",
+    "SATURDAY",
+  ];
   // console.log(date.getTimezoneOffset())
   const [currentDate, setCurrentDate] = useState({
     hours: 0,
@@ -26,12 +26,12 @@ function App() {
     });
   });
 
-  // function Hour(h) {
-  //   if (h >= 12) {
-  //     return h - 12;
-  //   }
-  //   return h;
-  // }
+  function Hour(h) {
+    if (h >= 12) {
+      return h - 12;
+    }
+    return h;
+  }
 
   const amPm = currentDate.hours >= 12 ? "PM" : "AM";
 
@@ -44,18 +44,23 @@ function App() {
   }
   return (
     <>
-      <div className="head">
-       
-      </div>
-      
       <div className="App">
-        <div className="time">{Zero(currentDate.hours)}</div>
-        <span>:</span>
-        <div className="time">{Zero(currentDate.minutes)}</div>
-        <span>:</span>
-        <div className="time">{Zero(currentDate.seconds)}</div>
-        <span>:</span>
-        <span className="AMPM">{amPm}</span>
+        <div className="head">
+          <h1>Digital - Clock</h1>
+          <h1 className="day">
+            {" "}
+            <strong> {days[date.getDay()]} </strong>{" "}
+          </h1>
+        </div>
+        <div className="timeContainer">
+          <div className="time">{Hour(Zero(currentDate.hours))}</div>
+          <span className="firstSpan">:</span>
+          <div className="time">{Zero(currentDate.minutes)}</div>
+          <span className="secondSpan">:</span>
+          <div className="time seconds">{Zero(currentDate.seconds)}</div>
+          <span className="thirdSpan">:</span>
+          <span className="AMPM">{amPm}</span>
+        </div>
       </div>
       {/* <div className="dayDiv">
         <span className="day">{days[date.getDay()]}</span>
@@ -65,3 +70,4 @@ function App() {
 }
 
 export default App;
+
